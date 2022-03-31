042acb4b2f5934569694982e4b0592868140ba3f
209410678 <209410678@gms.tku.edu.tw>
2022-03-31 19:47:36 +0800

    w6-p3

---
+++ classdemo/w06/Step1_Routing_Pages/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+import './index.css';
+import App_78 from './App_78';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App_78 />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
