a8bb0df3481ffe675694a7476d98da0e01076b9c
209410678 <209410678@gms.tku.edu.tw>
2022-03-31 21:07:01 +0800

    w6-p4

--- client_78/src/pages/Register_78.js
+++ client_78/src/pages/Register_78.js
@@ -1,8 +1,62 @@
+import { useState, useEffect } from 'react';
+import { Logo_78, FormRow_78 } from '../components';
+import Wrapper from '../assets/wrappers/Register_78';
+
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    isMember: true,
+    showAlert: false
+}
+
 function Register_78() {
+    const [values, setValues] = useState(initialState);
+
+    const handleChange = (e) => {
+        console.log('e.target', e.target)
+        setValues({...values, [e.target.name]: e.target.value });
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log('e.target', e.target)
+    }
+
   return (
-    <div>
-      Register
-    </div>
+    <Wrapper>
+      <form className="form" onSubmit={onSubmit}>
+          <Logo_78 />
+          <h3>Register</h3>
+          {/* name input */}
+          <FormRow_78 
+            type="text"
+            name='name'
+            value={values.name}
+            handleChange={handleChange}
+            className='form-input' 
+          />
+          {/*email input */}
+          <FormRow_78 
+            type="email"
+            name='email'
+            value={values.email}
+            handleChange={handleChange}
+            className='form-input' 
+          />
+          {/*password input */}
+          <FormRow_78 
+            type="password"
+            name='password'
+            value={values.password}
+            handleChange={handleChange}
+            className='form-input' 
+          />
+          <button className='btn btn-block' type="submit">
+              submit
+          </button>
+      </form>
+    </Wrapper>
   )
 }
 
