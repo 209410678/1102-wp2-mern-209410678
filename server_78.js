5047214db217c63c623e7e0efbb492883206d9d1
209410678 <209410678@gms.tku.edu.tw>
2022-04-14 20:16:59 +0800

    w08-p2 connect MongoDB server, w08-p3 Use MongoCompass to create database demo_78 and collection user,enter one document that has your name and sid

--- server_78.js
+++ server_78.js
@@ -2,9 +2,12 @@
 //const express = require('express');
 
 import express from 'express';
-import { allowedNodeEnvironmentFlags } from 'process';
-
 const app = express();
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import connectDB_78 from './db/connect_78.js'
 
 app.get('/',(req, res) => {
     res.send('Welcome 鄭芷琳 209410678');
@@ -12,4 +15,17 @@ app.get('/',(req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+const start = async () => {
+    try{
+        await connectDB_78(process.env.MONGO_LOCAL_URL).then( () => {
+            console.log('Connecting to MongoDB');
+        });
+        app.listen(port, () => console.log(`Server is running on port ${port}`));
+    }catch(err){
+        console.log(err);
+    }
+
+}
+
+start();
+
