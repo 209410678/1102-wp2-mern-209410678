1ea2b0a373200ad197936851744a251b004e150b
209410678 <li201217n@gmail.com>
2022-05-19 21:58:22 +0800

    w13

--- controllers/authController_78.js
+++ controllers/authController_78.js
@@ -1,13 +1,25 @@
-const register_78 = async (req, res) => {
-    res.send('register user -- 鄭芷琳, 209410678');
+import User_78 from '../models/User_78.js';
+
+const register_78 = async (req, res, next) => {
+  try {
+    console.log('body', req.body);
+    const user = await User_78.create(req.body);
+    const token = user.createJWT();
+    res.status(201).json({ user, token });
+  } catch (err) {
+    //res.status(500).json({ msg: 'Error on registering user' });
+    next(err);
+  }
+
+  //res.send('register user -- 鄭芷琳, 209410678');
 };
 
 const login_78 = async (req, res) => {
-    res.send('login user -- 鄭芷琳, 209410678');
+  res.send('login user -- 鄭芷琳, 209410678');
 };
 
 const updateUser_78 = async (req, res) => {
-    res.send('update user -- 鄭芷琳, 209410678');
+  res.send('update user -- 鄭芷琳, 209410678');
 };
 
 export { register_78, login_78, updateUser_78 };
--- middleware/error-handler_78.js
+++ middleware/error-handler_78.js
@@ -1,6 +1,6 @@
 const errorHandlerMiddleware_78 = (err, req, res, next) => {
-    console.log('error', err);
-    res.status(500).json({ msg: 'there wss an error' });
-}
+  console.log('error', err);
+  res.status(500).json({ msg: err });
+};
 
 export default errorHandlerMiddleware_78;
